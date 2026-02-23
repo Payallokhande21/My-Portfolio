d7e50845051c3a9e12c56e4fdd5d02897ef34bb4
Payal lokhande <lpayal012@gmail.com>
2026-02-23 20:52:21 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,14 @@
+ // Scroll Reveal Animation
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+  const trigger = window.innerHeight / 1.3;
+
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+
+    if(top < trigger){
+      section.classList.add('show');
+    }
+  });
+});
